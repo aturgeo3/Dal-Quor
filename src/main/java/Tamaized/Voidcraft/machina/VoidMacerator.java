cc2ea6bb0d633507d96908eb4ca666d6c2f0f7b9
Tamaized <ryanolt123@gmail.com>
2016-05-01 13:14:01 -0700

    cleanup

--- src/main/java/Tamaized/Voidcraft/machina/VoidMacerator.java
+++ src/main/java/Tamaized/Voidcraft/machina/VoidMacerator.java
@@ -39,7 +39,7 @@ public class VoidMacerator extends BasicVoidBlockContainer {
 	
 	public VoidMacerator(String string){
 		super(Material.rock, string);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(ACTIVE, Boolean.valueOf(false)));
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(ACTIVE, Boolean.valueOf(false)));
 	}
 	
 	@Override
@@ -53,42 +53,40 @@ public class VoidMacerator extends BasicVoidBlockContainer {
 	
 	@Override
 	protected BlockState createBlockState(){
-        return new BlockState(this, new IProperty[]{FACING, ACTIVE});
-    }
+		return new BlockState(this, new IProperty[]{FACING, ACTIVE});
+	}
 	
 	@Override
 	public IBlockState getStateFromMeta(int meta){
 		int val = meta > 5 ? meta-6 : meta;
 		EnumFacing enumfacing = EnumFacing.getFront(val);
-
-        if (enumfacing.getAxis() == EnumFacing.Axis.Y)
-        {
-            enumfacing = EnumFacing.NORTH;
-        }
-
-        return this.getDefaultState().withProperty(FACING, enumfacing).withProperty(ACTIVE, meta > 5);
-    }
+		
+		if (enumfacing.getAxis() == EnumFacing.Axis.Y){
+			enumfacing = EnumFacing.NORTH;
+		}
+		
+		return this.getDefaultState().withProperty(FACING, enumfacing).withProperty(ACTIVE, meta > 5);
+	}
 	
 	@SideOnly(Side.CLIENT)
-    public IBlockState getStateForEntityRender(IBlockState state){
-        return this.getDefaultState().withProperty(FACING, EnumFacing.SOUTH).withProperty(ACTIVE, false);
-    }
-
-    /**
-     * Convert the BlockState into the correct metadata value
-     */
+	public IBlockState getStateForEntityRender(IBlockState state){
+		return this.getDefaultState().withProperty(FACING, EnumFacing.SOUTH).withProperty(ACTIVE, false);
+	}
+	
+	/**
+	 * Convert the BlockState into the correct metadata value
+	 */
 	@Override
-    public int getMetaFromState(IBlockState state){
+	public int getMetaFromState(IBlockState state){
 		int meta = ((EnumFacing)state.getValue(FACING)).getIndex();
 		if(state.getValue(ACTIVE)) meta = meta + 6;
 		return meta;
-    }
+	}
 	
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void randomDisplayTick(World world, BlockPos pos, IBlockState state, Random rand){
 		if(state.getValue(ACTIVE)){
-			
 			float x1 = (float) pos.getX() + 0.5F;
 			float y1 = (float) pos.getY() + rand.nextFloat(); 
 			float z1 = (float) pos.getZ() + 0.5F;
@@ -124,58 +122,46 @@ public class VoidMacerator extends BasicVoidBlockContainer {
 	}
 	
 	private void setDefaultFacing(World world, BlockPos pos, IBlockState state){
-		if (!world.isRemote)
-        {
-            Block block = world.getBlockState(pos.north()).getBlock();
-            Block block1 = world.getBlockState(pos.south()).getBlock();
-            Block block2 = world.getBlockState(pos.west()).getBlock();
-            Block block3 = world.getBlockState(pos.east()).getBlock();
-            EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
-
-            if (enumfacing == EnumFacing.NORTH && block.isFullBlock() && !block1.isFullBlock())
-            {
-                enumfacing = EnumFacing.SOUTH;
-            }
-            else if (enumfacing == EnumFacing.SOUTH && block1.isFullBlock() && !block.isFullBlock())
-            {
-                enumfacing = EnumFacing.NORTH;
-            }
-            else if (enumfacing == EnumFacing.WEST && block2.isFullBlock() && !block3.isFullBlock())
-            {
-                enumfacing = EnumFacing.EAST;
-            }
-            else if (enumfacing == EnumFacing.EAST && block3.isFullBlock() && !block2.isFullBlock())
-            {
-                enumfacing = EnumFacing.WEST;
-            }
-
-            world.setBlockState(pos, state.withProperty(FACING, enumfacing).withProperty(ACTIVE, state.getValue(ACTIVE)), 2);
-        }
+		if (!world.isRemote){
+			Block block = world.getBlockState(pos.north()).getBlock();
+			Block block1 = world.getBlockState(pos.south()).getBlock();
+			Block block2 = world.getBlockState(pos.west()).getBlock();
+			Block block3 = world.getBlockState(pos.east()).getBlock();
+			EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
+			
+			if (enumfacing == EnumFacing.NORTH && block.isFullBlock() && !block1.isFullBlock()){
+				enumfacing = EnumFacing.SOUTH;
+			}else if (enumfacing == EnumFacing.SOUTH && block1.isFullBlock() && !block.isFullBlock()){
+				enumfacing = EnumFacing.NORTH;
+			}else if (enumfacing == EnumFacing.WEST && block2.isFullBlock() && !block3.isFullBlock()){
+				enumfacing = EnumFacing.EAST;
+			}else if (enumfacing == EnumFacing.EAST && block3.isFullBlock() && !block2.isFullBlock()){
+				enumfacing = EnumFacing.WEST;
+			}
+			
+			world.setBlockState(pos, state.withProperty(FACING, enumfacing).withProperty(ACTIVE, state.getValue(ACTIVE)), 2);
+		}
 	}
 	
 	public static void setState(boolean active, World worldIn, BlockPos pos){
-        IBlockState iblockstate = worldIn.getBlockState(pos);
-        TileEntity tileentity = worldIn.getTileEntity(pos);
-        TileEntityVoidMacerator te = (TileEntityVoidMacerator) tileentity;
-
-        if (active)
-        {
-            worldIn.setBlockState(pos, voidCraft.blocks.voidMacerator.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)).withProperty(ACTIVE, active), 3);
-            worldIn.setBlockState(pos, voidCraft.blocks.voidMacerator.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)).withProperty(ACTIVE, active), 3);
-        }
-        else
-        {
-            worldIn.setBlockState(pos, voidCraft.blocks.voidMacerator.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)).withProperty(ACTIVE, active), 3);
-            worldIn.setBlockState(pos, voidCraft.blocks.voidMacerator.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)).withProperty(ACTIVE, active), 3);
-        }
-
-
-        if (tileentity != null)
-        {
-            tileentity.validate();
-            worldIn.setTileEntity(pos, tileentity);
-        }
-    }
+		IBlockState iblockstate = worldIn.getBlockState(pos);
+		TileEntity tileentity = worldIn.getTileEntity(pos);
+		TileEntityVoidMacerator te = (TileEntityVoidMacerator) tileentity;
+		
+		if (active){
+			worldIn.setBlockState(pos, voidCraft.blocks.voidMacerator.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)).withProperty(ACTIVE, active), 3);
+			worldIn.setBlockState(pos, voidCraft.blocks.voidMacerator.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)).withProperty(ACTIVE, active), 3);
+		}else{
+			worldIn.setBlockState(pos, voidCraft.blocks.voidMacerator.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)).withProperty(ACTIVE, active), 3);
+			worldIn.setBlockState(pos, voidCraft.blocks.voidMacerator.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)).withProperty(ACTIVE, active), 3);
+		}
+		
+		
+		if (tileentity != null){
+			tileentity.validate();
+			worldIn.setTileEntity(pos, tileentity);
+		}
+	}
 	
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side, float hitX, float hitY, float hitZ){
@@ -187,9 +173,9 @@ public class VoidMacerator extends BasicVoidBlockContainer {
 	
 	@Override
 	public int getRenderType(){
-        return 3;
-    }
-		
+		return 3;
+	}
+	
 	@Override
 	public TileEntity createNewTileEntity(World arg0, int arg1) {
 		return new TileEntityVoidMacerator();
@@ -236,46 +222,42 @@ public class VoidMacerator extends BasicVoidBlockContainer {
 	
 	@Override
 	public void breakBlock(World world, BlockPos pos, IBlockState state){
-			TileEntityVoidMacerator tileentity = (TileEntityVoidMacerator) world.getTileEntity(pos);
-			
-			if(tileentity != null){
-				for(int i = 0; i < tileentity.getSizeInventory(); i++){
-					ItemStack itemstack = tileentity.getStackInSlot(i);
+		TileEntityVoidMacerator tileentity = (TileEntityVoidMacerator) world.getTileEntity(pos);
+		
+		if(tileentity != null){
+			for(int i = 0; i < tileentity.getSizeInventory(); i++){
+				ItemStack itemstack = tileentity.getStackInSlot(i);
+				
+				if(itemstack != null){
+					float f = this.rand.nextFloat() * 0.8F + 0.1F;
+					float f1 = this.rand.nextFloat() * 0.8F + 0.1F;
+					float f2 = this.rand.nextFloat() * 0.8F + 0.1F;
 					
-					if(itemstack != null){
-						float f = this.rand.nextFloat() * 0.8F + 0.1F;
-						float f1 = this.rand.nextFloat() * 0.8F + 0.1F;
-						float f2 = this.rand.nextFloat() * 0.8F + 0.1F;
+					while(itemstack.stackSize > 0){
+						int j = this.rand.nextInt(21);
 						
-						while(itemstack.stackSize > 0){
-							int j = this.rand.nextInt(21);
-							
-							if(j > itemstack.stackSize){
-								j = itemstack.stackSize;
-							}
-							
-							itemstack.stackSize-=j;
-							
-							EntityItem item = new EntityItem(world, (double)((float)pos.getX() + f), (double)((float)pos.getY() + f1), (double)((float)pos.getZ() + f2), new ItemStack(itemstack.getItem()));
-							
-							if(itemstack.hasTagCompound()){
-								item.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
-							}
-							
-							float f3 = 0.05F;
-							item.motionX = (double)((float)this.rand.nextGaussian() * f3);
-							item.motionY = (double)((float)this.rand.nextGaussian() * f3 + 0.2F);
-							item.motionZ = (double)((float)this.rand.nextGaussian() * f3);
-							
-							world.spawnEntityInWorld(item);
+						if(j > itemstack.stackSize){
+							j = itemstack.stackSize;
 						}
+						
+						itemstack.stackSize-=j;
+						
+						EntityItem item = new EntityItem(world, (double)((float)pos.getX() + f), (double)((float)pos.getY() + f1), (double)((float)pos.getZ() + f2), new ItemStack(itemstack.getItem()));
+						
+						if(itemstack.hasTagCompound()){
+							item.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
+						}
+						
+						float f3 = 0.05F;
+						item.motionX = (double)((float)this.rand.nextGaussian() * f3);
+						item.motionY = (double)((float)this.rand.nextGaussian() * f3 + 0.2F);
+						item.motionZ = (double)((float)this.rand.nextGaussian() * f3);
+						
+						world.spawnEntityInWorld(item);
 					}
 				}
-				
-				//world.func_96440_m(x, y, z, oldBlockID);
 			}
-		
-		
+		}
 		super.breakBlock(world, pos, state);
 	}	
 }
