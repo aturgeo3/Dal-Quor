3e12c71875222a3528fbe9419ca488b97db332a2
Tamaized <ryanolt123@gmail.com>
2016-05-02 18:45:49 -0700

    overlay render

--- src/main/java/Tamaized/Voidcraft/events/client/OverlayEvent.java
+++ src/main/java/Tamaized/Voidcraft/events/client/OverlayEvent.java
@@ -10,13 +10,19 @@ import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import Tamaized.Voidcraft.common.voidCraft;
 import Tamaized.Voidcraft.handlers.PortalDataHandler;
 
 public class OverlayEvent {
+	
+	private static final String texture_void = (voidCraft.modid+":blocks/blockPortalVoid.png");
+	private static final String texture_xia = (voidCraft.modid+":blocks/blockPortalXia.png");
 	
 	@SubscribeEvent
 	public void InGameOverlay(RenderGameOverlayEvent e){
@@ -27,8 +33,6 @@ public class OverlayEvent {
 			if(e.type == e.type.PORTAL){
 				float j = voidCraft.instance.VoidTickEvent.data.get(player.getGameProfile().getId()).tick;
 				int type = voidCraft.instance.VoidTickEvent.data.get(player.getGameProfile().getId()).type;
-		        IBlockState b = type == PortalDataHandler.PORTAL_VOID ? voidCraft.blocks.blockPortalVoid.getDefaultState() : type == PortalDataHandler.PORTAL_XIA ? voidCraft.blocks.blockPortalXia.getDefaultState() : null;
-		        if(b == null) return;
 				ScaledResolution scaledRes = new ScaledResolution(mc);
 				GlStateManager.disableAlpha();
 		        GlStateManager.disableDepth();
@@ -36,11 +40,13 @@ public class OverlayEvent {
 		        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
 		        GlStateManager.color(1.0F, 1.0F, 1.0F, j);
 		        mc.getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
-		        TextureAtlasSprite textureatlassprite = mc.getBlockRendererDispatcher().getBlockModelShapes().getTexture(b);
+		        
+		        TextureAtlasSprite textureatlassprite = mc.getBlockRendererDispatcher().getBlockModelShapes().getModelManager().getTextureMap().getAtlasSprite(type == PortalDataHandler.PORTAL_XIA ?  texture_xia : texture_void);
 		        float f = textureatlassprite.getMinU();
 		        float f1 = textureatlassprite.getMinV();
 		        float f2 = textureatlassprite.getMaxU();
 		        float f3 = textureatlassprite.getMaxV();
+		        
 		        Tessellator tessellator = Tessellator.getInstance();
 		        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
 		        worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX);
