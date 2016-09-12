ec912b2987ac6e9576c2ea83018f1eb8746283b7
Tamaized <ryanolt123@gmail.com>
2016-09-11 22:27:12 -0700

    0.9.0_DEV03

--- src/main/java/Tamaized/Voidcraft/voidCraft.java
+++ src/main/java/Tamaized/Voidcraft/voidCraft.java
@@ -95,7 +95,7 @@ import Tamaized.Voidcraft.world.dim.Xia.WorldProviderXia;
 @Mod(modid = voidCraft.modid, name = "VoidCraft", version = voidCraft.version, dependencies = "required-before:" + TamModized.modid + "@[" + TamModized.version + ",)")
 public class voidCraft extends TamModBase {
 
-	public static final String version = "0.9.0_DEV02";
+	public static final String version = "0.9.0_DEV03";
 	public static final String modid = "voidcraft";
 
 	public static String getVersion() {
