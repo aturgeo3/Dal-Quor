1a14e26209958ffeef181934d1b8871222db8996
Tamaized <ryanolt123@gmail.com>
2016-10-05 11:01:29 -0700

    fixed #40

--- src/main/java/Tamaized/Voidcraft/potion/PotionVoidicInfusionImmunity.java
+++ src/main/java/Tamaized/Voidcraft/potion/PotionVoidicInfusionImmunity.java
@@ -1,27 +1,18 @@
 package Tamaized.Voidcraft.potion;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Field;
-
 import Tamaized.Voidcraft.voidCraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PotionVoidicInfusionImmunity extends Potion {
 
 	private final ResourceLocation iconTexture;
-
-	@SideOnly(Side.CLIENT)
-	private static final MethodHandle Z_LEVEL = findFieldGetter(net.minecraft.client.gui.Gui.class, "zLevel", "field_73735_i");
 
 	public PotionVoidicInfusionImmunity(String name) {
 		super(false, 0x7700FF);
@@ -62,16 +53,6 @@ public class PotionVoidicInfusionImmunity extends Potion {
 	public void renderHUDEffect(int x, int y, PotionEffect effect, net.minecraft.client.Minecraft mc, float alpha) {
 		mc.getTextureManager().bindTexture(iconTexture);
 		net.minecraft.client.gui.Gui.drawModalRectWithCustomSizedTexture(x + 4, y + 4, 0, 0, 16, 16, 16, 16);
-	}
-
-	public static MethodHandle findFieldGetter(Class<?> clazz, String... fieldNames) {
-		final Field field = ReflectionHelper.findField(clazz, fieldNames);
-
-		try {
-			return MethodHandles.lookup().unreflectGetter(field);
-		} catch (IllegalAccessException e) {
-			throw new ReflectionHelper.UnableToAccessFieldException(fieldNames, e);
-		}
 	}
 
 }
