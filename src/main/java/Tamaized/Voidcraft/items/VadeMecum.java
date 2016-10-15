01b8f6a2874a5af4359f3a70f20ca20500a6d407
Tamaized <ryanolt123@gmail.com>
2016-10-15 02:40:12 -0700

    trig particles

--- src/main/java/Tamaized/Voidcraft/items/VadeMecum.java
+++ src/main/java/Tamaized/Voidcraft/items/VadeMecum.java
@@ -8,8 +8,11 @@ import Tamaized.Voidcraft.voidCraft;
 import Tamaized.Voidcraft.GUI.GuiHandler;
 import Tamaized.Voidcraft.capabilities.CapabilityList;
 import Tamaized.Voidcraft.capabilities.vadeMecum.IVadeMecumCapability;
+import Tamaized.Voidcraft.events.client.DebugEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -29,8 +32,7 @@ public class VadeMecum extends TamItem {
 	}
 
 	@Override
-	public EnumActionResult onItemUseFirst(ItemStack stack, EntityPlayer player, World world, BlockPos pos,
-			EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
+	public EnumActionResult onItemUseFirst(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
 		if (player.isSneaking()) {
 			player.getCapability(CapabilityList.VADEMECUM, null).toggleBookState();
 			return EnumActionResult.FAIL;
@@ -38,7 +40,7 @@ public class VadeMecum extends TamItem {
 			IVadeMecumCapability cap = player.getCapability(CapabilityList.VADEMECUM, null);
 			if (cap != null && cap.getBookState()) {
 				RayTraceResult result = RayTraceHelper.tracePath(world, player, 32, 1, new HashSet<Entity>());
-				if(result != null){
+				if (result != null) {
 					BlockPos bp = result.getBlockPos();
 					EntityLightningBolt entitylightningbolt = new EntityLightningBolt(world, bp.getX(), bp.getY(), bp.getZ(), false);
 					world.addWeatherEffect(entitylightningbolt);
@@ -50,35 +52,56 @@ public class VadeMecum extends TamItem {
 	}
 
 	@Override
-	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos,
-			EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-		if (!worldIn.isRemote)
-			openBook(playerIn, worldIn, playerIn.getPosition());
+	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if (!worldIn.isRemote) openBook(playerIn, worldIn, playerIn.getPosition());
 		return super.onItemUse(stack, playerIn, worldIn, pos, hand, facing, hitX, hitY, hitZ);
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn,
-			EnumHand hand) {
-		if (!worldIn.isRemote)
-			openBook(playerIn, worldIn, playerIn.getPosition());
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		if (!worldIn.isRemote) openBook(playerIn, worldIn, playerIn.getPosition());
 		return super.onItemRightClick(itemStackIn, worldIn, playerIn, hand);
 	}
 
 	private void openBook(EntityPlayer player, World world, BlockPos pos) {
-		player.openGui(voidCraft.instance, GuiHandler.getTypeID(GuiHandler.Type.VadeMecum), world, pos.getX(),
-				pos.getY(), pos.getZ());
+		player.openGui(voidCraft.instance, GuiHandler.getTypeID(GuiHandler.Type.VadeMecum), world, pos.getX(), pos.getY(), pos.getZ());
 	}
-	
+
 	@Override
 	public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected) {
-		if(world.isRemote){
-			IVadeMecumCapability cap = entity.getCapability(CapabilityList.VADEMECUM, null);
-			if(cap != null && cap.getBookState()){
-				double xOffset = 0.5D;
-				double yOffset = 0.5D;
-				double zOffset = 0.0D;
-				world.spawnParticle(EnumParticleTypes.PORTAL, entity.getPosition().getX()+xOffset, entity.getPosition().getY()+yOffset, entity.getPosition().getZ()+zOffset, 0, 1, 0);
+		if (world.isRemote && entity instanceof EntityLivingBase && isSelected) {
+			EntityLivingBase living = (EntityLivingBase) entity;
+			IVadeMecumCapability cap = living.getCapability(CapabilityList.VADEMECUM, null);
+			if (cap != null && cap.getBookState()) {
+				boolean flag = false;
+				if (entity == Minecraft.getMinecraft().thePlayer) {
+					flag = Minecraft.getMinecraft().gameSettings.thirdPersonView == 0;
+				}
+				if (flag) {
+
+					double pitch = living.rotationPitch < 0 ? 0 : (90 - living.rotationPitch);
+					double yaw = Math.toRadians(living.rotationYaw + 10 + (pitch / 2));
+
+					double range = 0.15D + ((pitch / 90) / 2);
+					DebugEvent.debugMode = true;
+					DebugEvent.textL = "pitch: " + pitch + "; range: " + range;
+
+					double xOffset = (range * -Math.cos(yaw));
+					double yOffset = 0.5D;
+					double zOffset = (range * -Math.sin(yaw));
+
+					world.spawnParticle(EnumParticleTypes.PORTAL, living.posX + xOffset, living.posY + yOffset, living.posZ + zOffset, 0, 0, 0);
+				} else {
+					double yaw = Math.toRadians(living.renderYawOffset - 50);
+
+					double range = 0.63D;
+
+					double xOffset = (range * -Math.cos(yaw));
+					double yOffset = 0.25D;
+					double zOffset = (range * -Math.sin(yaw));
+
+					world.spawnParticle(EnumParticleTypes.PORTAL, living.posX + xOffset, living.posY + yOffset, living.posZ + zOffset, 0, 0, 0);
+				}
 			}
 		}
 	}
