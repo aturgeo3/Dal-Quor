5796f0595bcc37dc545542e9f15553cb2cdafea1
Tamaized <ryanolt123@gmail.com>
2016-10-23 13:24:56 -0700

    also i'm a retard

--- src/main/java/Tamaized/Voidcraft/api/voidicpower/VoidicPowerItem.java
+++ src/main/java/Tamaized/Voidcraft/api/voidicpower/VoidicPowerItem.java
@@ -70,7 +70,6 @@ public abstract class VoidicPowerItem extends TamItem {
 
 	@Override
 	public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected) {
-		stack.getSubCompound(key, create)
 		IVoidicPowerCapability cap = stack.getCapability(CapabilityList.VOIDICPOWER, null);
 		if (cap != null) {
 			if (cap.isDefault()) {
