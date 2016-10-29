74183ed09d7574e127418632a03093feb2c641c3
Tamaized <ryanolt123@gmail.com>
2016-10-29 13:33:08 -0700

    Hole in Reality Vade Mecum entry is more clear

--- src/main/java/Tamaized/Voidcraft/vadeMecum/contents/documentation/blocks/realityhole/VadeMecumPageListRealityHole.java
+++ src/main/java/Tamaized/Voidcraft/vadeMecum/contents/documentation/blocks/realityhole/VadeMecumPageListRealityHole.java
@@ -9,7 +9,7 @@ public class VadeMecumPageListRealityHole implements IVadeMecumPageProvider {
 
 	public IVadeMecumPage[] getPageList(IVadeMecumCapability cap) {
 		return new IVadeMecumPage[] {
-				new VadeMecumPage("Hole in Reality", "This Rift in Space links back to the Overworld. Stepping into one will result in being teleported to around the same coordinates from where you were in the void. Being around these rifts reminds you of the sensation you feel when you're being watched by... him...")
+				new VadeMecumPage("Hole in Reality", "This Rift in Space can link to many dimensions. Stepping into one will result in being teleported to one of these dimensions, at random, around the same coordinates from where you were in the void, give or take ~200 blocks. Being around these rifts reminds you of the sensation you feel when you're being watched by... him...")
 				};
 	}
 
