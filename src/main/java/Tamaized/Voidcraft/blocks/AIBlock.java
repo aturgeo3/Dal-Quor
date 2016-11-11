179cfbef7d40ee4cf5ea653792d7ed9b035cdfd0
Tamaized <ryanolt123@gmail.com>
2016-11-10 17:35:38 -0800

    pillar texture

--- src/main/java/Tamaized/Voidcraft/blocks/AIBlock.java
+++ src/main/java/Tamaized/Voidcraft/blocks/AIBlock.java
@@ -9,51 +9,70 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import Tamaized.TamModized.blocks.TamBlockContainer;
 import Tamaized.Voidcraft.voidCraft;
 import Tamaized.Voidcraft.blocks.tileentity.TileEntityAIBlock;
 
 public class AIBlock extends TamBlockContainer {
-	
+
 	public static final PropertyInteger STATE = PropertyInteger.create("state", 0, 4);
-	
+
 	public AIBlock(String string) {
 		super(null, Material.CLOTH, string, -1);
 		setBlockUnbreakable();
 		setDefaultState(this.blockState.getBaseState().withProperty(STATE, 0));
 	}
-	
-	public static TileEntityAIBlock getMyTileEntity(World world, BlockPos pos){
+
+	public static TileEntityAIBlock getMyTileEntity(World world, BlockPos pos) {
 		Block b = world.getBlockState(pos.add(0, -1, 0)).getBlock();
-		if(b instanceof AIBlock){
+		if (b instanceof AIBlock) {
 			return ((AIBlock) b).getMyTileEntity(world, pos.add(0, -1, 0));
 		}
 		TileEntity te = world.getTileEntity(pos);
-		if(te instanceof TileEntityAIBlock) return (TileEntityAIBlock) te;
+		if (te instanceof TileEntityAIBlock) return (TileEntityAIBlock) te;
 		else return null;
 	}
-	
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.CUTOUT;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
 	@Override
 	public TileEntity createNewTileEntity(World world, int meta) {
 		return new TileEntityAIBlock();
 	}
-	
+
 	@Override
-	public IBlockState onBlockPlaced(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer){
+	public IBlockState onBlockPlaced(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
 		world.setBlockToAir(pos);
-        return this.getStateFromMeta(meta);
-    }
-	
+		return this.getStateFromMeta(meta);
+	}
+
 	@Override
-	public void breakBlock(World world, BlockPos pos, IBlockState state){
+	public void breakBlock(World world, BlockPos pos, IBlockState state) {
 		TileEntity te = world.getTileEntity(pos);
-		if(te instanceof TileEntityAIBlock){
+		if (te instanceof TileEntityAIBlock) {
 			TileEntityAIBlock teAI = (TileEntityAIBlock) te;
 		}
-		
+
 		super.breakBlock(world, pos, state);
 	}
 
@@ -74,8 +93,7 @@ public class AIBlock extends TamBlockContainer {
 	}
 
 	/**
-	 * Override this and return 'new BlockState(this, new IProperty[0])' if
-	 * hasAxis is false
+	 * Override this and return 'new BlockState(this, new IProperty[0])' if hasAxis is false
 	 */
 	@Override
 	protected BlockStateContainer createBlockState() {
