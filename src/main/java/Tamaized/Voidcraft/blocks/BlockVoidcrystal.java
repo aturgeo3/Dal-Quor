360d9be8d7f96d42a7fa4f8d927bc40c4ef14bfb
Tamaized <ryanolt123@gmail.com>
2017-04-29 10:57:37 -0700

    cherry-pick: fix Void Crystal Block render

--- src/main/java/Tamaized/Voidcraft/blocks/BlockVoidcrystal.java
+++ src/main/java/Tamaized/Voidcraft/blocks/BlockVoidcrystal.java
@@ -4,12 +4,17 @@ import java.util.Random;
 
 import Tamaized.TamModized.blocks.TamBlock;
 import Tamaized.Voidcraft.VoidCraft;
+import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -48,6 +53,13 @@ public class BlockVoidcrystal extends TamBlock {
 	@SideOnly(Side.CLIENT)
 	public BlockRenderLayer getBlockLayer() {
 		return BlockRenderLayer.TRANSLUCENT;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+		IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
+		Block block = iblockstate.getBlock();
+		return block == this ? false : super.shouldSideBeRendered(blockState, blockAccess, pos, side);
 	}
 
 }
