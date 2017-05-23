249c55663d442224c604924e7fc2b543e056ddae
Tamaized <ryanolt123@gmail.com>
2017-05-22 23:30:16 -0700

    some quick animation prep

--- src/main/java/Tamaized/Voidcraft/entity/boss/lob/EntityLordOfBlades.java
+++ src/main/java/Tamaized/Voidcraft/entity/boss/lob/EntityLordOfBlades.java
@@ -5,17 +5,75 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import Tamaized.Voidcraft.entity.EntityVoidBoss;
+import Tamaized.Voidcraft.entity.EntityVoidNPC;
+import Tamaized.Voidcraft.entity.boss.lob.render.ModelLordOfBlades;
+import Tamaized.Voidcraft.entity.client.animation.AnimationRegistry;
+import Tamaized.Voidcraft.entity.client.animation.IAnimation;
+import Tamaized.Voidcraft.entity.client.animation.AnimatableModel.AnimatableModelArms;
 import Tamaized.Voidcraft.network.IVoidBossAIPacket;
 import Tamaized.Voidcraft.xiaCastle.logic.battle.EntityVoidNPCAIBase;
 import Tamaized.Voidcraft.xiaCastle.logic.battle.IBattleHandler;
 import io.netty.buffer.ByteBufInputStream;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 
 public class EntityLordOfBlades extends EntityVoidBoss<IBattleHandler> {
+	
+	public static final int animations = AnimationRegistry.register(AnimationTest.class);
 
+	public static class AnimationTest implements IAnimation<EntityLordOfBlades, ModelLordOfBlades> {
+		
+		public static enum Type {
+			Idle, Attack, Spell, Spin, Fly, Land, Charge
+		}
+		
+		public static void play(EntityLordOfBlades entity, Type type){
+			AnimationTest animation = ((AnimationTest) entity.constructAnimation(animations));
+			entity.setAnimation(animation);
+			entity.playAnimation();
+		}
+
+		// Degrees
+		private float leftArmYaw = 0.0f;
+		private float leftArmPitch = 0.0f;
+		private float rightArmYaw = 0.0f;
+		private float rightArmPitch = 0.0f;
+
+		private int tick = 20 * 2;
+
+		@Override
+		public boolean update(EntityLordOfBlades e) {
+			tick--;
+			return tick <= 0;
+		}
+
+		@Override
+		public void render(EntityLordOfBlades e, ModelLordOfBlades model) {
+			model.setAnimations(leftArmPitch, rightArmPitch, leftArmYaw, rightArmYaw);
+		}
+
+		@Override
+		public void encodePacket(DataOutputStream stream) throws IOException {
+			stream.writeFloat(leftArmYaw);
+			stream.writeFloat(leftArmPitch);
+			stream.writeFloat(rightArmYaw);
+			stream.writeFloat(rightArmPitch);
+		}
+
+		@Override
+		public void decodePacket(ByteBufInputStream stream) throws IOException {
+			leftArmYaw = stream.readFloat();
+			leftArmPitch = stream.readFloat();
+			rightArmYaw = stream.readFloat();
+			rightArmPitch = stream.readFloat();
+		}
+
+	}
+	
 	public EntityLordOfBlades(World world) {
 		super(world, new IBattleHandler() {
 
@@ -131,6 +189,11 @@ public class EntityLordOfBlades extends EntityVoidBoss<IBattleHandler> {
 	@Override
 	protected int maxPhases() {
 		return 1;
+	}
+	
+	@Override
+	protected boolean processInteract(EntityPlayer player, EnumHand hand) {
+		return super.processInteract(player, hand);
 	}
 
 	@Override
