37560c792392645ff26b600306dca4c36c5cbd8a
Tamaized <ryanolt123@gmail.com>
2017-06-03 00:38:27 -0700

    some packet safety

--- src/main/java/Tamaized/Voidcraft/network/ClientPacketHandler.java
+++ src/main/java/Tamaized/Voidcraft/network/ClientPacketHandler.java
@@ -1,7 +1,5 @@
 package Tamaized.Voidcraft.network;
 
-import java.io.IOException;
-
 import Tamaized.TamModized.helper.MotionHelper;
 import Tamaized.Voidcraft.api.voidicpower.VoidicPowerItem;
 import Tamaized.Voidcraft.capabilities.CapabilityList;
@@ -25,127 +23,135 @@ import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientCustomPacketE
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.io.IOException;
+
 public class ClientPacketHandler {
 
-	public static enum PacketType {
-		INFUSION_UPDATE, INFUSION_UPDATE_ALL, ENTITY_UPDATES, ANIMATIONS, SHEATHE, PLAYER_MOTION, VADEMECUM_UPDATE, VOIDICPOWERITEM, GHOSTPLAYER_UPDATES, CLIENT_HEALTH
-	}
+    public static int getPacketTypeID(PacketType type) {
+        return type.ordinal();
+    }
 
-	public static int getPacketTypeID(PacketType type) {
-		return type.ordinal();
-	}
+    public static PacketType getPacketTypeFromID(int id) {
+        return PacketType.values()[id];
+    }
 
-	public static PacketType getPacketTypeFromID(int id) {
-		return PacketType.values()[id];
-	}
+    @SideOnly(Side.CLIENT)
+    public static void processPacket(ByteBuf parBB) throws IOException {
+        World world = Minecraft.getMinecraft().world;
+        if (world == null)
+            return;
+        ByteBufInputStream bbis = new ByteBufInputStream(parBB);
+        int pktType = bbis.readInt();
+        switch (getPacketTypeFromID(pktType)) {
+            case CLIENT_HEALTH: {
+                Minecraft.getMinecraft().player.setHealth(bbis.readFloat());
+            }
+            break;
+            case GHOSTPLAYER_UPDATES: {
+                Entity entity = world.getEntityByID(bbis.readInt());
+                if (entity instanceof EntityGhostPlayerBase) {
+                    ((EntityGhostPlayerBase) entity).decodePacket(bbis);
+                }
+            }
+            break;
+            case VOIDICPOWERITEM: {
+                int slot = bbis.readInt();
+                ItemStack stack = ItemStackNetworkHelper.decodeStack(parBB, bbis);
+                ItemStack checkStack = ItemStack.EMPTY;
+                switch (slot) {
+                    case VoidicPowerItem.PLAYER_INV_SLOT_OFFHAND:
+                        checkStack = Minecraft.getMinecraft().player.inventory.offHandInventory.get(0);
+                        break;
+                    case VoidicPowerItem.PLAYER_INV_SLOT_ARMOR_HELM:
+                        checkStack = Minecraft.getMinecraft().player.inventory.armorInventory.get(0);
+                        break;
+                    case VoidicPowerItem.PLAYER_INV_SLOT_ARMOR_CHEST:
+                        checkStack = Minecraft.getMinecraft().player.inventory.armorInventory.get(1);
+                        break;
+                    case VoidicPowerItem.PLAYER_INV_SLOT_ARMOR_LEGS:
+                        checkStack = Minecraft.getMinecraft().player.inventory.armorInventory.get(2);
+                        break;
+                    case VoidicPowerItem.PLAYER_INV_SLOT_ARMOR_BOOTS:
+                        checkStack = Minecraft.getMinecraft().player.inventory.armorInventory.get(3);
+                        break;
+                    default:
+                        checkStack = Minecraft.getMinecraft().player.inventory.mainInventory.get(slot);
+                        break;
+                }
+                if (checkStack.isEmpty() || stack.isEmpty() || !ItemStack.areItemStacksEqual(checkStack, stack))
+                    break;
+                IVoidicPowerCapability cap = checkStack.getCapability(CapabilityList.VOIDICPOWER, null);
+                if (cap != null) {
+                    cap.decodePacket(bbis);
+                }
+            }
+            break;
+            case VADEMECUM_UPDATE: {
+                IVadeMecumCapability vadeMecumCap = Minecraft.getMinecraft().player.getCapability(CapabilityList.VADEMECUM, null);
+                if (vadeMecumCap != null)
+                    vadeMecumCap.decodePacket(parBB, bbis);
+            }
+            break;
+            case ANIMATIONS: {
+                Entity entity = world.getEntityByID(bbis.readInt());
+                if (entity instanceof EntityVoidNPC) {
+                    EntityVoidNPC npc = (EntityVoidNPC) entity;
+                    IAnimation animation = npc.constructAnimation(bbis.readInt());
+                    if (animation != null)
+                        animation.decodePacket(bbis);
+                }
+            }
+            break;
+            case ENTITY_UPDATES: {
+                Entity entity = world.getEntityByID(bbis.readInt());
+                if (entity instanceof IEntitySync)
+                    ((IEntitySync) entity).decodePacket(bbis);
+            }
+            break;
+            case SHEATHE: {
+                Entity entity = world.getEntityByID(bbis.readInt());
+                if (entity instanceof EntityLivingBase) {
+                    EntityLivingBase living = (EntityLivingBase) entity;
+                    living.addPotionEffect(new PotionEffect(Potion.getPotionById(bbis.readInt()), bbis.readInt()));
+                }
+            }
+            break;
+            case INFUSION_UPDATE: {
+                Entity entity = world.getEntityByID(bbis.readInt());
+                if (entity != null && entity instanceof EntityLivingBase) {
+                    IVoidicInfusionCapability cap = entity.getCapability(CapabilityList.VOIDICINFUSION, null);
+                    if (cap != null)
+                        cap.decodePacket(bbis);
+                }
+            }
+            break;
+            case PLAYER_MOTION: {
+                MotionHelper.updatePlayerMotion(bbis.readDouble(), bbis.readDouble(), bbis.readDouble());
+            }
+            break;
+            default: {
 
-	@SubscribeEvent
-	@SideOnly(Side.CLIENT)
-	public void onClientPacket(ClientCustomPacketEvent event) {
-		Minecraft.getMinecraft().addScheduledTask(new Runnable() {
-			public void run() {
-				try {
-					processPacket(event.getPacket().payload());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+            }
+            break;
+        }
+        bbis.close();
+    }
 
-	@SideOnly(Side.CLIENT)
-	public static void processPacket(ByteBuf parBB) throws IOException {
-		World world = Minecraft.getMinecraft().world;
-		if (world == null) return;
-		ByteBufInputStream bbis = new ByteBufInputStream(parBB);
-		int pktType = bbis.readInt();
-		switch (getPacketTypeFromID(pktType)) {
-			case CLIENT_HEALTH: {
-				Minecraft.getMinecraft().player.setHealth(bbis.readFloat());
-			}
-				break;
-			case GHOSTPLAYER_UPDATES: {
-				Entity entity = world.getEntityByID(bbis.readInt());
-				if (entity instanceof EntityGhostPlayerBase) {
-					((EntityGhostPlayerBase) entity).decodePacket(bbis);
-				}
-			}
-				break;
-			case VOIDICPOWERITEM: {
-				int slot = bbis.readInt();
-				ItemStack stack = ItemStackNetworkHelper.decodeStack(parBB, bbis);
-				ItemStack checkStack = ItemStack.EMPTY;
-				switch (slot) {
-					case VoidicPowerItem.PLAYER_INV_SLOT_OFFHAND:
-						checkStack = Minecraft.getMinecraft().player.inventory.offHandInventory.get(0);
-						break;
-					case VoidicPowerItem.PLAYER_INV_SLOT_ARMOR_HELM:
-						checkStack = Minecraft.getMinecraft().player.inventory.armorInventory.get(0);
-						break;
-					case VoidicPowerItem.PLAYER_INV_SLOT_ARMOR_CHEST:
-						checkStack = Minecraft.getMinecraft().player.inventory.armorInventory.get(1);
-						break;
-					case VoidicPowerItem.PLAYER_INV_SLOT_ARMOR_LEGS:
-						checkStack = Minecraft.getMinecraft().player.inventory.armorInventory.get(2);
-						break;
-					case VoidicPowerItem.PLAYER_INV_SLOT_ARMOR_BOOTS:
-						checkStack = Minecraft.getMinecraft().player.inventory.armorInventory.get(3);
-						break;
-					default:
-						checkStack = Minecraft.getMinecraft().player.inventory.mainInventory.get(slot);
-						break;
-				}
-				if (checkStack.isEmpty() || stack.isEmpty() || !ItemStack.areItemStacksEqual(checkStack, stack)) break;
-				IVoidicPowerCapability cap = checkStack.getCapability(CapabilityList.VOIDICPOWER, null);
-				if (cap != null) {
-					cap.decodePacket(bbis);
-				}
-			}
-				break;
-			case VADEMECUM_UPDATE: {
-				IVadeMecumCapability vadeMecumCap = Minecraft.getMinecraft().player.getCapability(CapabilityList.VADEMECUM, null);
-				if (vadeMecumCap != null) vadeMecumCap.decodePacket(parBB, bbis);
-			}
-				break;
-			case ANIMATIONS: {
-				Entity entity = world.getEntityByID(bbis.readInt());
-				if (entity instanceof EntityVoidNPC) {
-					EntityVoidNPC npc = (EntityVoidNPC) entity;
-					IAnimation animation = npc.constructAnimation(bbis.readInt());
-					if (animation != null) animation.decodePacket(bbis);
-				}
-			}
-				break;
-			case ENTITY_UPDATES: {
-				Entity entity = world.getEntityByID(bbis.readInt());
-				if (entity instanceof IEntitySync) ((IEntitySync) entity).decodePacket(bbis);
-			}
-				break;
-			case SHEATHE: {
-				Entity entity = world.getEntityByID(bbis.readInt());
-				if (entity instanceof EntityLivingBase) {
-					EntityLivingBase living = (EntityLivingBase) entity;
-					living.addPotionEffect(new PotionEffect(Potion.getPotionById(bbis.readInt()), bbis.readInt()));
-				}
-			}
-				break;
-			case INFUSION_UPDATE: {
-				Entity entity = world.getEntityByID(bbis.readInt());
-				if (entity != null && entity instanceof EntityLivingBase) {
-					IVoidicInfusionCapability cap = entity.getCapability(CapabilityList.VOIDICINFUSION, null);
-					if (cap != null) cap.decodePacket(bbis);
-				}
-			}
-				break;
-			case PLAYER_MOTION: {
-				MotionHelper.updatePlayerMotion(bbis.readDouble(), bbis.readDouble(), bbis.readDouble());
-			}
-				break;
-			default: {
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onClientPacket(ClientCustomPacketEvent event) {
+        Minecraft.getMinecraft().addScheduledTask(new Runnable() {
+            public void run() {
+                try {
+                    processPacket(event.getPacket().payload());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-			}
-				break;
-		}
-		bbis.close();
-	}
+    public static enum PacketType {
+        INFUSION_UPDATE, INFUSION_UPDATE_ALL, ENTITY_UPDATES, ANIMATIONS, SHEATHE, PLAYER_MOTION, VADEMECUM_UPDATE, VOIDICPOWERITEM, GHOSTPLAYER_UPDATES, CLIENT_HEALTH
+    }
 }
