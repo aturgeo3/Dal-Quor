d78dc5eaebc476f62ddb1ba1842094bde9a3981c
Tamaized <ryanolt123@gmail.com>
2017-07-13 23:25:03 -0700

    god this is awful, i'll remove this system later

--- src/main/java/tamaized/voidcraft/network/VoidBossAIBus.java
+++ src/main/java/tamaized/voidcraft/network/VoidBossAIBus.java
@@ -1,13 +1,19 @@
 package tamaized.voidcraft.network;
 
-import java.util.ArrayList;
-
 import tamaized.voidcraft.common.xiacastle.logic.battle.EntityVoidNPCAIBase;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Each {@link tamaized.voidcraft.common.entity.EntityVoidBoss} has their own Bus
+ * This is used to notify each attached {@link EntityVoidNPCAIBase} of some event that occurs in the entity
+ * which allows each and every single one to act accordingly
+ */
 public class VoidBossAIBus {
 
-	private ArrayList<EntityVoidNPCAIBase> listeners = new ArrayList<EntityVoidNPCAIBase>();
-	private ArrayList<IVoidBossAIPacket> packetSpool = new ArrayList<IVoidBossAIPacket>();
+	private List<EntityVoidNPCAIBase> listeners = new ArrayList<>();
+	private List<IVoidBossAIPacket> packetSpool = new ArrayList<>();
 
 	/**
 	 * Packets are to be sent from the entity to the bus, ALL connected AIs will see and read the packet
@@ -17,24 +23,26 @@ public class VoidBossAIBus {
 	}
 
 	public void readNextPacket() {
-		if(packetSpool.isEmpty()) return;
+		if (packetSpool.isEmpty())
+			return;
 		IVoidBossAIPacket packet = packetSpool.get(0);
-		if (packet == null) return;
+		if (packet == null)
+			return;
 		for (EntityVoidNPCAIBase listener : listeners) {
 			listener.readPacket(packet);
 		}
 		packetSpool.remove(0);
 	}
-	
-	public void clearListeners(){
+
+	public void clearListeners() {
 		listeners.clear();
 	}
-	
-	public void addListener(EntityVoidNPCAIBase listener){
+
+	public void addListener(EntityVoidNPCAIBase listener) {
 		listeners.add(listener);
 	}
-	
-	public void removeListener(EntityVoidNPCAIBase listener){
+
+	public void removeListener(EntityVoidNPCAIBase listener) {
 		listeners.remove(listener);
 	}
 
