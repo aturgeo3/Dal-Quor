0b607a52e2ca5416191b9f850f98c3c5b106b430
Tamaized <ryanolt123@gmail.com>
2017-08-20 00:41:09 -0700

    why did i di this

--- src/main/java/tamaized/voidcraft/common/machina/Heimdall.java
+++ src/main/java/tamaized/voidcraft/common/machina/Heimdall.java
@@ -46,7 +46,7 @@ public class Heimdall extends TamBlockContainer {
 
 	@Override
 	public EnumBlockRenderType getRenderType(IBlockState state) {
-		return EnumBlockRenderType.INVISIBLE;
+		return EnumBlockRenderType.MODEL;
 	}
 
 	@Override
