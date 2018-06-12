b1db265b5b20de69db0e55d749816b99495c47ea
Tamaized <ryanolt123@gmail.com>
2018-06-11 23:36:10 -0700

    what in the actual fuck tbh
    I'll objcetholder this shit another day

--- src/main/java/tamaized/dalquor/registry/ModBiomes.java
+++ src/main/java/tamaized/dalquor/registry/ModBiomes.java
@@ -8,6 +8,7 @@ import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import tamaized.dalquor.DalQuor;
 import tamaized.dalquor.common.entity.mob.EntityMobWraith;
 import tamaized.dalquor.common.world.dim.dalquor.BiomeGenDream;
 import tamaized.dalquor.common.world.dim.thevoid.BiomeGenVoid;
@@ -21,7 +22,6 @@ public class ModBiomes {
 
 	public static Biome biomeVoid;
 	public static Biome biomeXia;
-
 	public static Biome biomeDreamOverworld;
 	public static Biome biomeDreamNether;
 	public static Biome biomeDreamEnd;
@@ -29,49 +29,49 @@ public class ModBiomes {
 
 	@SubscribeEvent
 	public static void register(RegistryEvent.Register<Biome> event) {
+		final String idVoid = "biome_void";
+		final String idXia = "biome_xia";
+		final String idDreamOverworld = "biome_dream_overworld";
+		final String idDreamNether = "biome_dream_nether";
+		final String idDreamEnd = "biome_dream_end";
+		final String idDreamVoid = "biome_dream_void";
+
 		float baseHeight = -1F;
 		float heightVariation = 0.1F;
-		BiomeProperties biomeVoidProp = new BiomeProperties("The Void").setBaseBiome("dalquor.biome_void").setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
-		BiomeProperties biomeXiaProp = new BiomeProperties("???").setBaseBiome("dalquor.biome_xia").setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
 
-		BiomeProperties biomeDreamOverworldProp = new BiomeProperties("Dream Overworld").setBaseBiome("dalquor.biome_dream_overworld").setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
-		BiomeProperties biomeDreamNetherProp = new BiomeProperties("Dream Nether").setBaseBiome("dalquor.biome_dream_nether").setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
-		BiomeProperties biomeDreamEndProp = new BiomeProperties("Dream End").setBaseBiome("dalquor.biome_dream_end").setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
-		BiomeProperties biomeDreamVoidProp = new BiomeProperties("Dream Void").setBaseBiome("dalquor.biome_dream_void").setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
+		BiomeProperties biomeVoidProp = makeProperties("The Void", idVoid).setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
+		BiomeProperties biomeXiaProp = makeProperties("???", idXia).setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
+		BiomeProperties biomeDreamOverworldProp = makeProperties("Dream Overworld", idDreamOverworld).setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
+		BiomeProperties biomeDreamNetherProp = makeProperties("Dream Nether", idDreamNether).setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
+		BiomeProperties biomeDreamEndProp = makeProperties("Dream End", idDreamEnd).setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
+		BiomeProperties biomeDreamVoidProp = makeProperties("Dream Void", idVoid).setBaseHeight(baseHeight).setHeightVariation(heightVariation).setTemperature(0.21F).setRainfall(0.0F).setRainDisabled();
 
 		biomeVoid = new BiomeGenVoid(biomeVoidProp);
 		biomeXia = new BiomeGenXia(biomeXiaProp);
-
 		biomeDreamOverworld = new BiomeGenDream(biomeDreamOverworldProp, new SpawnListEntry(EntityZombie.class, 100, 0, 1));
 		biomeDreamNether = new BiomeGenDream(biomeDreamNetherProp, new SpawnListEntry(EntityPigZombie.class, 100, 0, 1));
 		biomeDreamEnd = new BiomeGenDream(biomeDreamEndProp, new SpawnListEntry(EntityEnderman.class, 100, 0, 1));
 		biomeDreamVoid = new BiomeGenDream(biomeDreamVoidProp, new SpawnListEntry(EntityMobWraith.class, 100, 0, 1));
 
-		biomeVoid.setRegistryName("dalquor.biome_void");
-		biomeXia.setRegistryName("dalquor.biome_xia");
-
-		biomeDreamOverworld.setRegistryName("dalquor.biome_dream_overworld");
-		biomeDreamNether.setRegistryName("dalquor.biome_dream_nether");
-		biomeDreamEnd.setRegistryName("dalquor.biome_dream_end");
-		biomeDreamVoid.setRegistryName("dalquor.biome_dream_void");
-
-		event.getRegistry().register(biomeVoid);
-		event.getRegistry().register(biomeXia);
-
-		event.getRegistry().register(biomeDreamOverworld);
-		event.getRegistry().register(biomeDreamNether);
-		event.getRegistry().register(biomeDreamEnd);
-		event.getRegistry().register(biomeDreamVoid);
+		event.getRegistry().register(biomeVoid.setRegistryName(DalQuor.modid, idVoid));
+		event.getRegistry().register(biomeXia.setRegistryName(DalQuor.modid, idXia));
+		event.getRegistry().register(biomeDreamOverworld.setRegistryName(DalQuor.modid, idDreamOverworld));
+		event.getRegistry().register(biomeDreamNether.setRegistryName(DalQuor.modid, idDreamNether));
+		event.getRegistry().register(biomeDreamEnd.setRegistryName(DalQuor.modid, idDreamEnd));
+		event.getRegistry().register(biomeDreamVoid.setRegistryName(DalQuor.modid, idDreamVoid));
 	}
 
 	public static void init() {
 		BiomeDictionary.addTypes(biomeVoid, BiomeDictionary.Type.VOID);
 		BiomeDictionary.addTypes(biomeXia, BiomeDictionary.Type.VOID);
-
 		BiomeDictionary.addTypes(biomeDreamOverworld, BiomeDictionary.Type.VOID);
 		BiomeDictionary.addTypes(biomeDreamNether, BiomeDictionary.Type.VOID);
 		BiomeDictionary.addTypes(biomeDreamEnd, BiomeDictionary.Type.VOID);
 		BiomeDictionary.addTypes(biomeDreamVoid, BiomeDictionary.Type.VOID);
+	}
+
+	private static BiomeProperties makeProperties(String name, String id){
+		return new BiomeProperties(name).setBaseBiome(DalQuor.modid + ":" + id);
 	}
 
 }
